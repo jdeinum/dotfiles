dad5fa52c4586b38768477916d0a2f8f36d45b20
deinum <deinum@ualberta.ca>
2022-09-12 09:37:55 -0600

    auto-merged

---
+++ get_dotfiles.sh
@@ -0,0 +1,18 @@
+# nvim
+cp -r ~/.config/nvim .
+
+# xmonad + bar
+cp -r ~/.config/xmonad/xmonad.hs xmonad/xmonad.hs
+cp ~/.xmobarrc xmonad/.xmobarrc
+
+# zsh
+cp ~/.zshrc zsh/.zshrc
+
+# X11
+cp ~/.xinitrc X11/.xinitrc
+cp /etc/X11/xorg.conf.d/xorg.conf X11/xorg.conf.d
+
+# alacritty
+cp -r ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
+
+
